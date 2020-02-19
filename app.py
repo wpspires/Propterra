4123c2438e1d482fa52d0d5096fd759f98b3c91a
wpspires <treyspires@gmail.com>
2020-02-18 21:41:03 -0600

    Added some basic HTML framing and styling. Added static Maps frame

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import json
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
-
+Maps_API_Key = '12345'  #os.environ.get("Maps_API_Key")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", Maps_Key=Maps_API_Key)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
